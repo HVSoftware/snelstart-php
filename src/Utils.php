66504e9b92e0947f891b077b2d9df3405a0c37a0
Harry van der Valk <info@hvsoftware.nl>
2022-12-27 23:20:41 +0100

    Remove PaddingFound phpcs rule

--- src/Utils.php
+++ src/Utils.php
@@ -7,10 +7,6 @@ use GuzzleHttp\Exception\InvalidArgumentException;
 
 final class Utils
 {
-    private function __construct()
-    {
-    }
-
     /**
      * Source package: guzzlehttp/guzzle
      *
@@ -22,17 +18,19 @@ final class Utils
      * @param int    $depth   User specified recursion depth.
      * @param int    $options Bitmask of JSON decode options.
      *
-     * @return object|array|string|int|float|bool|null
-     *
      * @throws InvalidArgumentException if the JSON cannot be decoded.
      *
      * @link https://www.php.net/manual/en/function.json-decode.php
      */
-    public static function jsonDecode(string $json, bool $assoc = false, int $depth = 512, int $options = 0)
-    {
-        $data = \json_decode($json, $assoc, $depth, $options);
-        if (\JSON_ERROR_NONE !== \json_last_error()) {
-            throw new InvalidArgumentException('json_decode error: '.\json_last_error_msg());
+    public static function jsonDecode(
+        string $json,
+        bool $assoc = false,
+        int $depth = 512,
+        int $options = 0,
+    ): float|object|array|bool|int|string|null {
+        $data = json_decode($json, $assoc, $depth, $options);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new InvalidArgumentException('json_decode error: ' . json_last_error_msg());
         }
 
         return $data;
